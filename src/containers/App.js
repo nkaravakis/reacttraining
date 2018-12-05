e4dc8ac0df1ddfca27ee80c4bb92707b085d3130
Nick Karavakis <nkaravakis@gmail.com>
2018-12-05 14:29:42 -0700

    Completed Section 7 Lecture 90 (Performance Gains with PureComponents)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js] Inside Constructor', props)
@@ -25,10 +25,11 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount')
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
-    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState)
-    return true; //Check the pointer of the objects to see if they are the same, does not do a deep comparison
-  }
+  // shouldComponentUpdate (nextProps, nextState) {
+  //   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState)
+  //   return nextState.persons !== this.state.persons ||
+  //   nextState.showPersons !== this.state.showPersons; //Check the pointer of the objects to see if they are the same, does not do a deep comparison
+  // }
 
   componentWillUpdate (nextProps, nextState) {
     console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState)
@@ -85,6 +86,7 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
+            <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
             <Cockpit 
               appTitle={this.props.title}
               showPersons={this.state.showPersons}
