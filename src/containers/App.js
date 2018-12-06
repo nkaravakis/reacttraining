3feadf3033cc934a6c3297cbd77dc687b95d783a
Nick Karavakis <nkaravakis@gmail.com>
2018-12-06 13:47:05 -0700

    Completed Section 7 Lecture 99 (Using setState Correctly)

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,8 @@ class App extends PureComponent {
         { id: 'agfdsd23f', name: 'Manu', age: 29 },
         { id: 'sdfasdfsdf', name: 'Stephanie', age: 26 }],
         otherState: 'some other value',
-        showPersons: false    
+        showPersons: false,
+        toggleClicked: 0    
     }
   }
   
@@ -67,7 +68,12 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState( (prevState, props) => {
+      return {
+        showPersons: !doesShow, 
+        toggleClicked: prevState.toggleClicked + 1
+      }
+      })
   }
 
   render() {    
