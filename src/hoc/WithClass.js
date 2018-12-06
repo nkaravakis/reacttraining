3889db7b7625a1caa764da13a2e3e04d50492040
Nick Karavakis <nkaravakis@gmail.com>
2018-12-06 14:24:38 -0700

    Completed Section 7 Lecture 103 (More on the React API (16.3) With Forwarded Refs)

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -9,15 +9,18 @@ import React, {Component} from 'react';
 // }
 
 const withClass = (WrappedComponent, className) => {
-    return class extends Component { //no name after "class" because its an anonymous class
+    const WithClass = class extends Component { //no name after "class" because its an anonymous class
         render () {
             return (
                 <div className={className}>
-                    <WrappedComponent {...this.props}/>
+                    <WrappedComponent ref={this.props.forwardedRef} {...this.props}/>
                 </div>
             )
         }
     }
+    return React.forwardRef((props, ref) => { //this is whatever `props` are being passed into Person component and whatever refs are being passed in via Persons. It forwards it to the underlying wrapped component 
+        return <WithClass {...props} forwardedRef={ref}/>;
+    })
 }
 
 export default withClass;
